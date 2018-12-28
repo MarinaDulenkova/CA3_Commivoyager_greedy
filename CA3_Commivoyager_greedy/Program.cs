ae813abe61bc507719432f8513680b8256305fe8
MarinaDulenkova <mdul97@mail.ru>
2018-12-28 14:40:21 +0300

    Project created

---
+++ CA3_Commivoyager_greedy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CA3_Commivoyager_greedy
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# CA3_Commivoyager_greedy" 
